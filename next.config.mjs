6a61324528916d9ea3c3f066194ca3d723f3b68d
raghav3615 <dadhichraghav896@gmail.com>
2025-05-19 23:54:30 +0530

    add image domains to next.config for profile pictures

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com', // Google profile pictures
+      'avatars.githubusercontent.com', // GitHub profile pictures
+    ],
+  },
   webpack: (config) => {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
